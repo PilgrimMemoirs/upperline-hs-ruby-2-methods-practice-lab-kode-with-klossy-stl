d6de44143fe5c4f3a83cc958e85c63ba5c5beb81
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-06-21 18:09:31 +0000

    Finished the Subway Method Lab

--- subway_methods.rb
+++ subway_methods.rb
@@ -5,7 +5,7 @@ def map_size
   "The map is #{21 * 25} square inches"
 end
 
-# Now you give it a shot! It takes about 156 seconds to go between stops and you'll be taking the train for 3 stops. Write a method that returns how long your trip will take. 
+# Now you give it a shot! It takes about 156 seconds to go between stops and you'll be taking the train for 3 stops. Write a method that returns how long your trip will take.
 def trip_length
 	# write your code here
 end
@@ -16,15 +16,16 @@ end
 
 
 
-# The train arrives and you hop on. Guess what time it is? It's showtime! There are 23 people on the train and each person gives the dancers 1.5 dollars. Write a method called pay_dancers that returns how much money they made. 
+# The train arrives and you hop on. Guess what time it is? It's showtime! There are 23 people on the train and each person gives the dancers 1.5 dollars. Write a method called pay_dancers that returns how much money they made.
 
 
 # There is one grumpy lady on the train that doesn't like the dancing though. Write a method called stop_dancing that returns a message to the dancers in all caps.
 
 
 # There is also a really enthusiastic rider who keeps shouting "Everything is awesome!" Write a method called everything_is_awesome that returns one string with "Everything is awesome!" 5 times. (Ruby has a `.times` method that can help you do this. Think about setting up an empty string and adding to it five times)
-
-
+def everything_is_awesome
+  return 5.times do"Everthing is awesome!"end
+end
 # You are almost at your stop and you start thinking about how you are going to get home. You have $18 dollars left on your metro card (For those of you not in New York, the price of a metro card is now $2.75). Write a method called remaining_trips that return how many trips you have left.
 
 
@@ -48,6 +49,7 @@ puts "That lady told the train dancers to"
 
 puts "That guy kept shouting"
 # call your method here
+puts everything_is_awesome
 
 puts "This is how many trips I have left on my metrocard"
 # call your method here
